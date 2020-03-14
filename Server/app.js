d6db922098addf1d6593789b321a0481f9e9827c
Gwendolyn Thomas <gwenthomas85@gmail.com>
2020-03-14 13:01:02 -0700

    changes

--- Server/app.js
+++ Server/app.js
@@ -9,6 +9,7 @@ var PORT = 3000;
 // Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Data
 var reservations = [
@@ -18,6 +19,36 @@ var reservations = [
 ];
 
 // Routing
+//Home Route//
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+//Reserve Table Route//
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/reserve.html"));
+});
+
+//waitlist Route//
+app.get("/waitlist", function(req, res) {
+  res.sendFile(path.join(__dirname, "waitlist.html"));
+});
+
+//API Routes go here
+app.get("/api/", function(req, res){
+  res.json()
+});
+
+app.get("/api/", function(req, res){
+  res.json()
+});
+
+//Post Requests//
+app.post("/api/", function(req, res) {
+
+  
+})
+
+//Port is listening//
+app.listen(PORT, () => {
+  console.log("App is listening at PORT:" + PORT)
+})
